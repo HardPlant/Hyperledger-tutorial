0258f1aab65100798c45886861c2016058ab2022
HardPlant <abc7988se@naver.com>
2018-05-01 04:47:11 -0700

    update
    Signed-off-by: HardPlant <abc7988se@naver.com>

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const composer = require('composer-client');
 bizNetworkConnection = new composer.BusinessNetworkConnection();
-var cardname = "";
+var cardname = "admin@blockchain-twitter";
+let BusinessNetworkDefinition = await bizNetworkConnection.connect(cardName);
 var id = 0;
 
 function getCurrentId(){
     return id++;
 }
 
-async function writeTwitt(user,message){
-    let BusinessNetworkDefinition = await bizNetworkConnection.connect(cardName);
+async function writeTwitt(message){
     var twittRegistry = await this.bizNetworkConnection.getAssetRegistry('org.blocktwitt.Tweet');
     let factory = this.BusinessNetworkDefinition.getFactory();
     let newTwitt = factory.newResource('org.blocktwitt.Tweet',
         "TweetId:" + getCurrentId())
     newTwitt.tweetId = Math.random()*10000000;
-    newTwitt.message = 
+    newTwitt.message = messsage;
+    
 }
+
