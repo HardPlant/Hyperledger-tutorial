e741d6c262cdf4fdad81b447ea6dfaa7d694cc92
HardPlant <abc7988se@naver.com>
2018-05-01 05:27:07 -0700

    update
    Signed-off-by: HardPlant <abc7988se@naver.com>

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const composer = require('composer-client');
 bizNetworkConnection = new composer.BusinessNetworkConnection();
-var cardname = "admin@blockchain-twitter";
-let BusinessNetworkDefinition = await bizNetworkConnection.connect(cardName);
+var cardName = "admin@blockchain-twitter";
 var id = 0;
 
 function getCurrentId(){
@@ -9,12 +8,36 @@ function getCurrentId(){
 }
 
 async function writeTwitt(message){
-    var twittRegistry = await this.bizNetworkConnection.getAssetRegistry('org.blocktwitt.Tweet');
+    let connect = await bizNetworkConnection.connect(cardName);
+    let twittRegistry = await bizNetworkConnection.getAssetRegistry('org.blocktwitt.Tweet');
     let factory = this.BusinessNetworkDefinition.getFactory();
     let newTwitt = factory.newResource('org.blocktwitt.Tweet',
         "TweetId:" + getCurrentId())
     newTwitt.tweetId = Math.random()*10000000;
     newTwitt.message = messsage;
-    
 }
+async function getIdentity(){
 
+}
+async function writeTwitt(){
+}
+async function readAllTwitts(){
+    let connect = await bizNetworkConnection.connect(cardName);
+    let twittRegistry = await bizNetworkConnection.getAssetRegistry('org.blocktwitt.Tweet');
+    let list = await twittRegistry.getAll()
+    console.log(list)
+}
+async function main(){
+    getIdentity();
+    return;
+}
+async function addUser(){
+    let connect = await bizNetworkConnection.connect(cardName);
+    let userRegistry = await bizNetworkConnection.getParticipantRegistry('org.blocktwitt.User');
+
+}
+try{
+    main()
+} catch(e){
+    console.log(e)
+}
